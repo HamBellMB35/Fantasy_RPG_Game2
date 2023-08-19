c3c4dd4eb19ae87081ee7c2c3cb8d37fb923d004
HamBellMB35 <hbrskates@gmail.com>
2023-08-19 08:28:40 -0300

    Worked on adding and removing targets from the target group when targetting

--- Assets/1Scripts/CombatScripts/CombatTargeting/Targeter.cs
+++ Assets/1Scripts/CombatScripts/CombatTargeting/Targeter.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,19 +13,22 @@ public class Targeter : MonoBehaviour
     public List<Target> targets = new List<Target>();                   // Remember to make private after testing
     public Target CurrentTarget {  get; private set; }
 
+    [SerializeField] private CinemachineTargetGroup targetGroup;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.TryGetComponent<Target>(out Target Target)) { return; }
+        if (!other.TryGetComponent<Target>(out Target target)) { return; }
 
-        targets.Add(Target);
+        targets.Add(target);
+        target.OnDestroyed += RemoveTarget;
 
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(!other.TryGetComponent<Target>(out Target Target)) { return; }
-        
-        targets.Remove(Target);
+        if(!other.TryGetComponent<Target>(out Target target)) { return; }
+
+        RemoveTarget(target);
   
     }
 
@@ -32,14 +36,33 @@ public class Targeter : MonoBehaviour
     {
         if (targets.Count == 0) { return false; }
       
-       CurrentTarget = targets[0];
+        CurrentTarget = targets[0];
+
+        targetGroup.AddMember(CurrentTarget.transform, 1f, 2f);
 
         return true;
     }
 
     public void CancelTargeting()
     {
+        if(CurrentTarget == null) { return; }
+
+        targetGroup.RemoveMember(CurrentTarget.transform);
+
+
         CurrentTarget = null;
+    }
+
+    private void RemoveTarget(Target target)
+    {
+        if(CurrentTarget == target)
+        {
+            targetGroup.RemoveMember(CurrentTarget.transform);
+            CurrentTarget = null;
+        }
+
+        target.OnDestroyed -= RemoveTarget;
+        targets.Remove(target);
     }
 
 
